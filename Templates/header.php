eb454089f61b6aa8a5203d6f38f2bb92aa19832f
Marcos Avila <107021643+MarcoAvlmt@users.noreply.github.com>
2024-05-30 15:01:15 -0600

    +

--- Templates/header.php
+++ Templates/header.php
@@ -1,3 +1,4 @@
+<?php $url_base = "http://localhost/V+TV/V-TV"; ?>
 <!doctype html>
 <html lang="en">
 
@@ -20,14 +21,14 @@
     </style>
     <nav class="navbar navbar-expand-lg bg-body-tertiary ">
         <div class="container-fluid">
-            <img src="Images/Icon.jpg" alt="Icono" width="80" height="50">
+            <img src="Images/Icon.png" alt="Icono" width="50" height="30">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">
+                        <a class="nav-link active" aria-current="page" href="<?php echo $url_base; ?>/index.php">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stars" viewBox="0 0 16 16">
                                 <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.73 1.73 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.73 1.73 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.73 1.73 0 0 0 3.407 2.31zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z" />
                             </svg> Inicio
@@ -65,18 +66,54 @@
                             </li>
                         </ul>
                     </li>
-                    <li class="nav-item">
+
+                    <!--li class="nav-item">
                         <a class="nav-link disabled" aria-disabled="true">Disabled</a>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+                    <input class="form-control me-2" type="search" placeholder="Buscar" disabled aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit" disabled >Buscar</button>
+                </form-->
             </div>
+            <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                        </svg> Perfil
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-vcard-fill" viewBox="0 0 16 16">
+                                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm9 1.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 0-1h-4a.5.5 0 0 0-.5.5M9 8a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 0-1h-4A.5.5 0 0 0 9 8m1 2.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 0-1h-3a.5.5 0 0 0-.5.5m-1 2C9 10.567 7.21 9 5 9c-2.086 0-3.8 1.398-3.984 3.181A1 1 0 0 0 2 13h6.96q.04-.245.04-.5M7 6a2 2 0 1 0-4 0 2 2 0 0 0 4 0" />
+                                </svg> Cuenta
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5M10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5" />
+                                </svg> Salir
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
         </div>
     </nav>
-
-
-
     <main class="container">
+        <!-- Contenido principal -->
+    </main>
+</body>
+
+</html>
+</div>
+</nav>
+<main class="container">
--- index.php
+++ index.php
@@ -1,3 +1,102 @@
 <?php include("Templates/header.php") ?>
-
+<!-- ESTE ES EL INICIO DE LA APP -->
+<div class="container"> <!-- Agregamos un contenedor para el contenido de la página -->
+    <div class="clock">
+        <div class="hour-hand"></div>
+        <div class="minute-hand"></div>
+        <div class="second-hand"></div>
+        <div class="center-circle"></div>
+    </div>
+</div>
 <?php include("templates/footer.php") ?>
+
+<style>
+    /* Estilos CSS */
+    body {
+        margin: 0;
+        background-color: #f0f0f0;
+    }
+
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: flex-start; /* Alineamos al inicio para estar cerca del navbar */
+        height: calc(10vh - 10px); /* Ajustamos la altura restando el alto del navbar */
+        padding-top: 10px; /* Añadimos un poco de espacio desde la parte superior */
+    }
+
+    .clock {
+        position: relative;
+        width: 200px;
+        height: 200px;
+        border: 10px solid #333;
+        border-radius: 50%;
+    }
+
+    .hour-hand,
+    .minute-hand,
+    .second-hand {
+        position: absolute;
+        background-color: #333;
+        transform-origin: center bottom; /* Cambiamos el origen de la transformación */
+    }
+
+    .hour-hand {
+        width: 4px;
+        height: 50px;
+        top: 50%;
+        left: 50%;
+        transform: translateX(-50%) translateY(-100%) rotate(0deg); /* Ajustamos la posición */
+    }
+
+    .minute-hand {
+        width: 3px;
+        height: 70px;
+        top: 50%;
+        left: 50%;
+        transform: translateX(-50%) translateY(-100%) rotate(0deg); /* Ajustamos la posición */
+    }
+
+    .second-hand {
+        width: 2px;
+        height: 90px;
+        top: 50%;
+        left: 50%;
+        transform: translateX(-50%) translateY(-100%) rotate(0deg); /* Ajustamos la posición */
+    }
+
+    .center-circle {
+        position: absolute;
+        width: 12px;
+        height: 12px;
+        background-color: #333;
+        border-radius: 50%;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
+
+<script>
+    // Script JavaScript
+    function setClock() {
+        const hourHand = document.querySelector('.hour-hand');
+        const minuteHand = document.querySelector('.minute-hand');
+        const secondHand = document.querySelector('.second-hand');
+
+        const date = new Date();
+        const hours = date.getHours() % 12; // Convertir a formato de 12 horas
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+
+        const hourDegrees = (hours * 30) + (0.5 * minutes); // 30 degrees per hour, 0.5 degrees per minute
+        const minuteDegrees = (minutes * 6) + (0.1 * seconds); // 6 degrees per minute, 0.1 degrees per second
+        const secondDegrees = seconds * 6; // 6 degrees per second
+
+        hourHand.style.transform = `translateX(-50%) translateY(-100%) rotate(${hourDegrees}deg)`; // Ajustamos la transformación
+        minuteHand.style.transform = `translateX(-50%) translateY(-100%) rotate(${minuteDegrees}deg)`; // Ajustamos la transformación
+        secondHand.style.transform = `translateX(-50%) translateY(-100%) rotate(${secondDegrees}deg)`; // Ajustamos la transformación
+    }
+    setInterval(setClock, 1000); // Actualizar cada segundo
+    setClock(); // Llamar inmediatamente para establecer la hora correcta al cargar
+</script>

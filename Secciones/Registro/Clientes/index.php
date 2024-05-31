cf0cae4d14c3b0668ae175a3cb1e7e56bfad8785
Marcos Avila <107021643+MarcoAvlmt@users.noreply.github.com>
2024-05-31 13:16:38 -0600

    Update index.php

--- Secciones/Registro/Clientes/index.php
+++ Secciones/Registro/Clientes/index.php
@@ -1,2 +1,51 @@
 <?php include("../../../Templates/header.php") ?>
+</br>
+</br>
+<div class="container-fluid">
+    <div class="row">
+        <!-- Contenedor para la tabla DATOS -->
+        <div class="col-9">
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Nombre</th>
+                            <th>Apellido</th>
+                            <th>Correo electrónico</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Juan</td>
+                            <td>Pérez</td>
+                            <td>juan.perez@example.com</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>María</td>
+                            <td>Gómez</td>
+                            <td>maria.gomez@example.com</td>
+                        </tr>
+                        <!-- Más filas según sea necesario -->
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <!-- Contenedor para el calendario CALENDARIOS -->
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Calendario</h5>
+                    <div id="calendar">
+                        <!-- Aquí podrías insertar un calendario interactivo utilizando una librería de JavaScript -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
 <?php include("../../../Templates/footer.php") ?>
